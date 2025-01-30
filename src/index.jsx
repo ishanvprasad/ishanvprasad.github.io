a49769daa995bffb8fb6f8ecc40b8b6d19f50b0a
Ishan Prasad <82628103+ishanvprasad@users.noreply.github.com>
2025-01-30 17:14:41 -0500

    added routing to starterpack.

---
+++ src/index.jsx
@@ -0,0 +1,54 @@
+import React, { useParams } from 'react';
+import { createRoot } from 'react-dom/client';
+import {
+  BrowserRouter, Routes, Route, NavLink,
+} from 'react-router';
+import './style.scss';
+
+function About(props) {
+  return <div> All there is to know about me </div>;
+}
+
+function Welcome(props) {
+  return <div>Welcome</div>;
+}
+
+function Test(props) {
+  const { id } = useParams();
+  return <div> ID: {id} </div>;
+}
+function FallBack(props) {
+  return <div>URL Not Found</div>;
+}
+
+function Nav(props) {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+}
+
+function App(props) {
+  return (
+    <BrowserRouter>
+      <div>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/test/:id" element={<Test />} />
+          <Route path="*" element={<FallBack />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+const root = createRoot(document.getElementById('main'));
+root.render(<App />);
