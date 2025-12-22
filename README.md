1ea32c9a56d99f5f77e6a2f1f85552f2c79fe0d2
Ishan Prasad <82628103+ishanvprasad@users.noreply.github.com>
2025-12-22 00:56:14 -0500

    clean slate

--- README.md
+++
@@ -1,3 +0,0 @@
-# ishanvprasad.github.io
-
-building website
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './style.scss';
-
-let num = 0;
-
-setInterval(() => {
-  num += 1;
-
-  document.getElementById('main').textContent = `You've been on this page for ${num} seconds.`;
-}, 1000);
